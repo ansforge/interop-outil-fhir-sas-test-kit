f1f1794bfd211642dff3e53e856391b3547ecb56
Arthur Oldrati <arthur.oldrati@esante.gouv.fr>
2026-03-04 08:41:04 +0000

    fix: regulator_id in conditional put modified to value of regulator_id_modif

--- lib/sas_test_kit/prise_de_rendez_vous/modification_nom_regulateur_test.rb
+++ lib/sas_test_kit/prise_de_rendez_vous/modification_nom_regulateur_test.rb
@@ -12,7 +12,7 @@ module MyTestKit
             updated_regulator = HelperFLuxv1.build_regulateur_body(regulator_id_modif, regulator_mail_modif, resource_id, regulator_first_name, regulator_last_name_modif, sys)
 
             http, url, headers = HelperFLuxv1.http_client(base_url)
-            url.query = URI.encode_www_form({ 'identifier': 'urn:oid:1.2.250.1.71.4.2.1|' + regulator_id })
+            url.query = URI.encode_www_form({ 'identifier': 'urn:oid:1.2.250.1.71.4.2.1|' + regulator_id_modif })
             response = http.put(url, updated_regulator.to_json, headers)
 
             add_message("info", "request : #{url}")
