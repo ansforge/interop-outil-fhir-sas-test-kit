7162922f1180f06b0018d8c511c5a5508fb8db9e
Arthur Oldrati <arthur.oldrati@esante.gouv.fr>
2026-04-27 07:53:43 +0000

    fix: slot_group_ps now use the same initialisation as all the other groups

--- lib/sas_test_kit/aggregation/slot_group_ps.rb
+++ lib/sas_test_kit/aggregation/slot_group_ps.rb
@@ -17,6 +17,14 @@ module SasTestKit
     )
     id :slot_group_ps
 
+    test from: :slot_search_setup do
+        config(
+            inputs: { 
+                practitioner_id: { name: :practitioner_id },
+            }
+        )
+    end
+
     test do
       title 'Test recherche par slot - renvoi Bundle'
       description %(
@@ -31,46 +39,13 @@ module SasTestKit
         L'objectif est de garantir que la recherche Slot fournit un résultat
         exploitable et conforme aux exigences de l'API.
       )
-
-      input :practitioner_id,
-            title: 'RPPS',
-            default: '810101603008'
-
       run do
-
-        #prévoir de gérer les time zone
-        formatted_start_date = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
-
-        three_days_later = Time.now + (3 * 24 * 60 * 60) # 3 days in seconds
-
-        # Cap at 72 hours (3 days)
-        max_limit = Time.now + (72 * 60 * 60)
-
-        # End of third day (23:59:59)
-        end_of_third_day = (Time.now + (2 * 24 * 60 * 60)).end_of_day rescue Time.new(Time.now.year, Time.now.month, Time.now.day + 2, 23, 59, 59)
-
-        # Final capped time
-        capped_time = [three_days_later, max_limit, end_of_third_day].min
-
-        # Format capped time
-        formatted_end_date = capped_time.strftime("%Y-%m-%dT%H:%M:%S")
-    
-        hash = {
-        _include: 'Slot:schedule', 
-        '_include:iterate': 'Schedule:actor',
-        'schedule.actor:Practitioner.identifier': 'urn:oid:1.2.250.1.71.4.2.1|'+ practitioner_id,
-        start: ["ge#{formatted_start_date}.000+00", "le#{formatted_end_date}.000+00"],
-        status: 'free'
-        }
-
-        mTLS == 'true' ? fhir_search('Slot', params: hash) : fhir_search('Slot', params: hash, client: :no_mTLS)  
+        bundle = scratch[:Bundle]
+        skip "Le test d'initialisation doit être validé pour évaluer ce test" if (!bundle)
         
-        assert_response_status(200)
-        assert_resource_type('Bundle')
-      
-        assert_valid_resource(profile_url: 'http://sas.fr/fhir/StructureDefinition/BundleAgregateur', validator: :validator_sas)  
+        assert_resource_type('Bundle', resource: bundle)
+        assert_valid_resource(resource: bundle, profile_url: 'http://sas.fr/fhir/StructureDefinition/BundleAgregateur', validator: :validator_sas)  
       end
     end
-
   end
 end
