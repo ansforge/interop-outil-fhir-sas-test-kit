751dfb768e176125b8ebf9ceec436ceacf08bd5c
Arthur Oldrati <arthur.oldrati@esante.gouv.fr>
2026-05-04 07:57:50 +0000

    refactor: multi_lieux_group has been seprated between tests files and group file

--- lib/sas_test_kit/aggregation/aggregation_group.rb
+++ lib/sas_test_kit/aggregation/aggregation_group.rb
@@ -1,10 +1,13 @@
 require_relative 'single-practitioner-single-location_group'
-require_relative 'multi_lieux_group_ps'
+require_relative 'multi_lieux_group'
 require_relative 'options_slot_group'
 require_relative 'performance_group'
 require_relative 'practitioner_group_optionnel_ps'
 require_relative 'search_multiple_ps_group'
 require_relative 'slot_group'
+require_relative 'organizational_group_optionnel'
+
+require_relative '../sas_options.rb'
 
 module SasTestKit
     class AggregationGroup < Inferno::TestGroup
@@ -17,7 +20,7 @@ module SasTestKit
 
         group from: :single_practitioner_single_location
 
-        group from: :multiLieu_group_ps
+        group from: :multi_lieux_group
 
         group from: :search_multiple_ps_group
 
@@ -26,5 +29,8 @@ module SasTestKit
         group from: :optionslots_group
             
         group from: :performance_group
+
+        group from: :orga_optionnel_group,
+            required_suite_options: SASOptions::IG_REQUIREMENT_CPTS
     end
 end
