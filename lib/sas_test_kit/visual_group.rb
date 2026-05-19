2f245b9345ebbf46f7fb624e0dd4f145bf88a3cb
Arthur Oldrati <arthur.oldrati@esante.gouv.fr>
2026-05-19 13:25:13 +0000

    fix: Improved visual_group

--- lib/sas_test_kit/visual_group.rb
+++ lib/sas_test_kit/visual_group.rb
@@ -1,10 +1,10 @@
 module SasTestKit
   class VisualGroup < Inferno::TestGroup
-    title 'Attestation (Prérequis, fonctionnalités, ...) '
+    title 'Tests déclaratifs (prérequis, fonctionnalités,...)'
     description 'Verification des prérequis et des  fonctionnalités du logiciel'
     id :visual_group
 
-    input_order :base_url, :mTLS, :gestion_rpps, :gestion_rpps_notes, :gestion_rpps_obligatoire, :gestion_rpps_obligatoire_notes, :gestion_idnst,:gestion_idnst_notes
+    input_order :gestion_rpps, :gestion_rpps_notes, :gestion_rpps_obligatoire, :gestion_rpps_obligatoire_notes, :gestion_idnst,:gestion_idnst_notes, :conformite_hds, :conformite_hds_notes, :authentification_mfa, :authentification_mfa_notes, :access_restriction, :access_restriction_notes
 
     test do
       title 'Gestion du RPPS pour les professionels de santé'
@@ -13,7 +13,7 @@ module SasTestKit
       )
       id 'Test01a'
       input :gestion_rpps,
-            title: 'Est-ce que logiciel utilise l\'IDNPS (RPPS) pour identifier les professionnels de santé ?',
+            title: 'Est-ce que la solution utilise l\'IDNPS (RPPS) pour identifier les professionnels de santé ?',
             type: 'radio',
             default: 'false',
             options: {
@@ -30,11 +30,11 @@ module SasTestKit
             }
       input :gestion_rpps_notes,
             title: 'Commentaire (en option)',
-            type: 'textarea',
+            type: 'text',
             optional: true
 
       run do
-        pass gestion_rpps_notes 
+        pass gestion_rpps_notes if gestion_rpps_notes.present?
       end
     end
 
@@ -65,11 +65,11 @@ module SasTestKit
         
       input :gestion_rpps_obligatoire_notes,
             title: 'Commentaire (en option)',
-            type: 'textarea',
+            type: 'text',
             optional: true
 
       run do
-        pass gestion_rpps_obligatoire_notes 
+        pass gestion_rpps_obligatoire_notes if gestion_rpps_obligatoire_notes.present?
         add_message('info', "Gestion RPPS obligatoire : " + gestion_rpps_obligatoire.to_s) 
       end
     end
@@ -81,7 +81,7 @@ module SasTestKit
       )
       id 'Test02'
       input :gestion_idnst,
-            title: 'Est-ce que logiciel utilise l\'IDNST (FINESS, SIRET,RRPS Rang) pour identifier les structures ?',
+            title: 'Est-ce que la solution utilise l\'IDNST (FINESS, SIRET,RRPS Rang) pour identifier les structures ?',
             type: 'radio',
             default: 'false',
             options: {
@@ -98,13 +98,106 @@ module SasTestKit
             }
       input :gestion_idnst_notes,
             title: 'Commentaire (en option)',
-            type: 'textarea',
+            type: 'text',
             optional: true
 
       run do
-        pass gestion_idnst_notes 
+        pass gestion_idnst_notes if gestion_idnst_notes.present?
       end
     end
 
+    test do
+      title 'Conformité HDS'
+      description %(
+          Vérification de la conformité aux règles d'hébergement HDS
+      )
+      id 'hds'
+      input :conformite_hds,
+            title: "Est-ce que la solution est conforme aux règles d'hergement HDS",
+            type: 'radio',
+            default: 'false',
+            options: {
+              list_options: [
+                {
+                  label: 'Oui',
+                  value: 'true'
+                },
+                {
+                  label: 'Non',
+                  value: 'false'
+                }
+              ]
+            }
+      input :conformite_hds_notes,
+            title: 'Commentaire (en option)',
+            type: 'text',
+            optional: true
+
+      run do
+        pass conformite_hds_notes if conformite_hds_notes.present?
+      end
+    end
+
+    test do
+      title 'Authentification multi-facteurs'
+      description %(
+          Vérification de la prise en charge de l'authentification multi-facteurs
+      )
+      id 'mfa'
+      input :authentification_mfa,
+            title: "Est ce que la solution met en place une authentification à deux facteurs pour les PS effecteurs ?",
+            type: 'radio',
+            default: 'false',
+            options: {
+              list_options: [
+                {
+                  label: 'Oui',
+                  value: 'true'
+                },
+                {
+                  label: 'Non',
+                  value: 'false'
+                }
+              ]
+            }
+      input :authentification_mfa_notes,
+            title: 'Commentaire (en option)',
+            type: 'text',
+            optional: true
+      run do
+        pass authentification_mfa_notes if authentification_mfa_notes.present?
+      end
+    end
+
+    test do
+      title "Restriction d'accès"
+      description %(
+          Vérification de la mise en place de restrictions d'accès après X tentatives de connexion infructueuses
+      )
+      id 'access_restriction'
+      input :access_restriction,
+            title: "Est ce que la solution met en place une restriction d'accès après X tentatives en erreurs ?",
+            type: 'radio',
+            default: 'false',
+            options: {
+              list_options: [
+                {
+                  label: 'Oui',
+                  value: 'true'
+                },
+                {
+                  label: 'Non',
+                  value: 'false'
+                }
+              ]
+            }
+      input :access_restriction_notes,
+            title: 'Commentaire (en option)',
+            type: 'text',
+            optional: true
+      run do
+        pass access_restriction_notes if access_restriction_notes.present?
+      end
+    end
   end
 end
