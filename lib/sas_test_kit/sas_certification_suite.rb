b01b3f319a76107446a0ea1854c88c52b57aa8df
Arthur Oldrati <arthur.oldrati@esante.gouv.fr>
2026-05-28 12:46:26 +0000

    feat: docker-compose-prod now has restart option set to 'always'

--- lib/sas_test_kit/sas_certification_suite.rb
+++ lib/sas_test_kit/sas_certification_suite.rb
@@ -105,16 +105,18 @@ module SasTestKit
 
     # All FHIR validation requests will use this FHIR validator
     fhir_resource_validator :validator_sas do
-       #igs 'ans.fhir.fr.sas#1.1.0' # Use this method for published IGs/versions
-       igs 'igs/sas_package.tgz'   # Use this otherwise
+       igs 'ans.fhir.fr.sas#1.1.0' # Use this method for published IGs/versions
+       #igs 'igs/sas_package.tgz'   # Use this otherwise
 
       exclude_message do |message|
-        message.message.match?(/\A\S+: \S+: URL value '.*' does not resolve/)
+        message.message.match?(/\A\S+: \S+: URL value '.*' does not resolve/) ||
+        message.message.include?(".specialty[0]: No code provided, and a code is required from the value set 'fr-practitioner-specialty'") ## Problème IG
       end
     end
+
     group from: :tls
 
-    group from: :visual_group     
+    group from: :visual_group
     
     group from: :capability_statement
 
